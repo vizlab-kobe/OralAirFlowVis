928bf408125599402c31144b6634cdf468a634c8
Naohisa Sakamoto <naohisa.sakamoto@gmail.com>
2022-11-10 13:49:42 +0900

    Modified.

--- realistic-s3/run_entropy.sh
+++ realistic-s3/run_entropy.sh
@@ -7,7 +7,7 @@ fi
 
 unset FOAM_SIGFPE
 #export FOAM_SIGFPE=false
-mpirun -n 8 ../${PROGRAM}_Entropy/$PROGRAM -parallel
+mpirun -n 8 ../${PROGRAM}_CameraPath/$PROGRAM -parallel
 
 #if type "ffmpeg" > /dev/null 2>&1; then
 #    ffmpeg -r 60 -start_number 000001 -i Output/output_%06d.bmp -vcodec libx264 -pix_fmt yuv420p -r 60 output.mp4
