1a22fa99ed88485247df09e61e60db7d4b0c5eca
Naohisa Sakamoto <naohisa.sakamoto@gmail.com>
2023-04-12 19:13:40 +0900

    Create run_camera_path.sh

---
+++ realistic-s2/run_camera_path.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+PROGRAM=rhoPimpleFoam
+
+if [ -z $WM_DIR ]; then
+    . ../etc/bashrc
+fi
+
+unset FOAM_SIGFPE
+#export FOAM_SIGFPE=false
+mpirun -n 48 ../${PROGRAM}_CameraPath/$PROGRAM -parallel
+
+#if type "ffmpeg" > /dev/null 2>&1; then
+#    ffmpeg -r 60 -start_number 000001 -i Output/output_%06d.bmp -vcodec libx264 -pix_fmt yuv420p -r 60 output.mp4
+#fi
